cb6133f2a52dc4084f97176b2b7c986697c6fe50
manfredArtavia <manfred.artavia-gomez@hpe.com>
2018-01-30 16:54:27 -0600

    components folder

---
+++ src/app/components/body.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-body',
+  templateUrl: './body.component.html'
+  // styleUrls: ['./header.component.css']
+})
+export class BodyComponent {
+  constructor() {
+  }
+}
